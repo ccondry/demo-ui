a849aa59f4345c7b2d1ca04b9d55b7935af8e15b
Coty Condry <ccondry@cisco.com>
2019-02-13 09:39:11 -0600

    log a better message when mutation type is undefined

--- client/store/actions.js
+++ client/store/actions.js
@@ -24,6 +24,12 @@ export const switchEffect = ({ commit }, data) => {
 }
 
 export const loadToState = async function ({getters, commit, dispatch}, options) {
+  if (!options.mutation) {
+    const message = 'error during loadToState ' + options.name + ' - options.mutation was undefined. Verify that the mutation type constant is in mutation-types.js'
+    console.log(message)
+    return
+    // throw Error(message)
+  }
   try {
     const response = await load(options.endpoint, options.query)
     console.log(`GET ${options.name}`, response)
