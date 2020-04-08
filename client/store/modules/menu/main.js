46de38a9db43ce2587c3b741e74e9e01949c2ecc
Coty Condry <ccondry@cisco.com>
2020-04-08 11:11:17 -0500

    remove commented menu item

--- client/store/modules/menu/main.js
+++ client/store/modules/menu/main.js
@@ -35,16 +35,5 @@ export default {
       },
       component: lazyLoading('main/upstream')
     }
-    // {
-    //   name: 'Verticals',
-    //   path: '/verticals/main/verticals',
-    //   meta: {
-    //     icon: 'copyright',
-    //     description: 'Custom branded demo website configuration',
-    //     created: '2018-10-29',
-    //     modified: '2018-10-29'
-    //   },
-    //   component: lazyLoading('main/verticals')
-    // }
   ]
 }
