a7335bba242480082de0e62bf244fa16d31f9223
Coty Condry <ccondry@cisco.com>
2020-09-02 13:33:19 -0500

    add defaults as a prop again

--- client/components/session-config.vue
+++ client/components/session-config.vue
@@ -111,6 +111,10 @@ export default {
     'model': {
       type: Object,
       default () { return {} }
+    },
+    'defaults': {
+      type: Object,
+      default () { return {} }
     }
   },
 
