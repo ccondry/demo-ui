3e10219e865935a9c4aaa612f4599e299c213212
Coty Condry <ccondry@cisco.com>
2021-09-08 11:16:05 -0500

    use window.location.href for cisco SSO, to match the referer value

--- client/store/modules/toolbox.js
+++ client/store/modules/toolbox.js
@@ -10,7 +10,8 @@ const getters = {
   oauthError: state => state.oauthError,
   ciscoRedirectUri () {
     // the URL to return to after completing SSO login
-    return `${window.location.protocol}//${window.location.host}/`
+    // return `${window.location.protocol}//${window.location.host}/`
+    return window.location.href
   },
   ciscoSsoUrl: (state, getters) => {
     // URL to forward user to when requesting SSO login
