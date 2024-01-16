d9349189e2f579958e5cfe1544e3d0bedd992c80
Coty Condry <ccondry@cisco.com>
2024-01-16 14:07:26 -0600

    building configure page

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/configure',
       name: 'configure',
       component: Configure
     },
